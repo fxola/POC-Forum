526f1bcf6b58411edd9626a43348256439dca737
fxola <afolabiabass62@gmmail.com>
2023-09-09 09:12:45 +0100

    chore: refactor api responses to reusable objects

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -10,6 +10,16 @@ import {
   updateUser,
 } from "../models/user";
 
+import {
+  EMAIL_TAKEN_ERROR,
+  successResponse,
+  USERNAME_TAKEN_ERROR,
+  VALIDATION_ERROR,
+  INTERNAL_SERVER_ERROR,
+  httpcode,
+  USER_NOT_FOUND_ERROR,
+} from "../models/response";
+
 export const createUser = async (
   req: Request<{}, {}, CreateUserDTO>,
   res: Response
@@ -18,32 +28,22 @@ export const createUser = async (
     const { email, lastName, firstName, username, password } = req.body;
 
     if (!email || !lastName || !firstName || !username) {
-      return res
-        .status(400)
-        .json({ error: "ValidationError", data: undefined, success: false });
+      return res.status(httpcode.BAD_REQUEST).json(VALIDATION_ERROR);
     }
 
     const validEmail = isValidEmail(email);
     if (!validEmail) {
-      return res
-        .status(400)
-        .json({ error: "ValidationError", data: undefined, success: false });
+      return res.status(httpcode.BAD_REQUEST).json(VALIDATION_ERROR);
     }
 
     const emailTaken = await emailExists({ email });
     if (emailTaken) {
-      return res
-        .status(409)
-        .json({ error: "EmailAlreadyInUse", data: undefined, success: false });
+      return res.status(httpcode.CONFLICT).json(EMAIL_TAKEN_ERROR);
     }
 
     const usernameTaken = await usernameExists({ username });
     if (usernameTaken) {
-      return res.status(409).json({
-        error: "UsernameAlreadyTaken",
-        data: undefined,
-        success: false,
-      });
+      return res.status(httpcode.CONFLICT).json(USERNAME_TAKEN_ERROR);
     }
 
     const user: CreateUserDTO = {
@@ -56,23 +56,17 @@ export const createUser = async (
 
     const userID = await saveUser(user);
 
-    return res.status(201).json({
-      error: undefined,
-      data: {
-        id: userID,
-        email,
-        username,
-        firstName,
-        lastName,
-      },
-      success: true,
+    const response = successResponse({
+      id: userID,
+      email,
+      username,
+      firstName,
+      lastName,
     });
+
+    return res.status(httpcode.CREATED).json(response);
   } catch (e) {
-    return res.json(500).json({
-      error: "InternalServerError",
-      success: false,
-      message: "Something went wrong",
-    });
+    return res.json(httpcode.SERVER_ERROR).json(INTERNAL_SERVER_ERROR);
   }
 };
 
@@ -86,41 +80,27 @@ export const editUser = async (
     const parsedId = Number(id);
 
     if (!parsedId) {
-      return res
-        .status(400)
-        .json({ error: "ValidationError", data: undefined, success: false });
+      return res.status(httpcode.BAD_REQUEST).json(VALIDATION_ERROR);
     }
 
     const user = await getUserById(parsedId);
     if (!user) {
-      return res
-        .status(404)
-        .json({ error: "UserNotFound", data: undefined, success: false });
+      return res.status(httpcode.NOT_FOUND).json(USER_NOT_FOUND_ERROR);
     }
 
     if (email) {
       if (!isValidEmail(email)) {
-        return res
-          .status(400)
-          .json({ error: "ValidationError", data: undefined, success: false });
+        return res.status(httpcode.BAD_REQUEST).json(VALIDATION_ERROR);
       }
 
       const emailTaken = await emailExists({ email });
       if (emailTaken) {
-        return res.status(409).json({
-          error: "EmailAlreadyInUse",
-          data: undefined,
-          success: false,
-        });
+        return res.status(httpcode.CONFLICT).json(EMAIL_TAKEN_ERROR);
       }
     }
 
     if (username && (await usernameExists({ username }))) {
-      return res.status(409).json({
-        error: "UsernameAlreadyTaken",
-        data: undefined,
-        success: false,
-      });
+      return res.status(httpcode.CONFLICT).json(USERNAME_TAKEN_ERROR);
     }
 
     const update: EditUserDTO = {
@@ -132,16 +112,11 @@ export const editUser = async (
     };
 
     const updatedUser = await updateUser(update);
+    const response = successResponse({ ...updatedUser });
 
-    return res
-      .status(200)
-      .json({ data: { ...updatedUser }, success: true, error: undefined });
+    return res.status(httpcode.OK).json(response);
   } catch (e) {
-    return res.json(500).json({
-      error: "InternalServerError",
-      success: false,
-      message: "Something went wrong",
-    });
+    return res.json(httpcode.SERVER_ERROR).json(INTERNAL_SERVER_ERROR);
   }
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,20 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes";
 import dotenv from "dotenv";
+import { NOT_FOUND_ERROR, httpcode } from "./models/response";
+
+const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 const app = express();
-app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
 app.use("/users", userRoutes);
+
 app.use((req, res, next) =>
-  next(
-    res
-      .status(400)
-      .json({
-        error: "Not Found",
-        success: false,
-        message: `${req.url} does not exist`,
-      })
-  )
+  next(res.status(httpcode.NOT_FOUND).json(NOT_FOUND_ERROR(req.url)))
 );
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
